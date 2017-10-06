1f1cda5be501d840a0237739431c83947bb96841
kl3ag1 <kl3ag1@komp17.lo1cg.org>
2017-10-06 09:39:41 +0200

    baza sql2

--- bazy/dane.py
+++ bazy/dane.py
@@ -5,21 +5,31 @@
 
 import csv
 
-def dane_z_pliku(plik):
+def dane_z_pliku(plik,delimiter='\t'):
     dane = [] #pusta lista
     with open(plik, 'r') as plikcsv:
-        tresc = csv.reader(plikcsv, delimiter='\t')
+        tresc = csv.reader(plikcsv, delimiter=delimiter)
     
         for rekord in tresc:
             dane.append(rekord)
         
-    print(dane)
+    return(dane)
+    
+def wyczysc_dane(dane,pole):
+    for i, rekord in enumerate(dane):
+        el = rekord[pole]
+        el = el.replace('zł','')
+        el = el.replace(' ','')
+        el = el.replace(',','.')
+        
+    return ' '
         
 
 def main(args):
     #dane_z_pliku('premia.txt')
     #dane_z_pliku('dział.txt')
-    dane_z_pliku('pracownicy.txt')
+    dane = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(dane,5)
     return 0
 
 if __name__ == '__main__':
